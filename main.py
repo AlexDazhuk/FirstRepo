77b4902cc8ab74d5e9ecd0f2b670a0fb649c6485
AlexDazhuk <alexdazhuk1@gmail.com>
2025-09-07 16:52:48 +0300

    del line

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
 a = 12
 b = 34
 print(a + b)
-print("branch test 1")
-print("branch test 2")
 print(a - b)
-print("branch test 3")
 print(a * b)
-print("branch test 4")
 print(a / b)
-print("branch test 5")
 print(a % b)
-print("branch test 6")
 print(a ** b)
-print("branch test 7")
-
 print(a // b)
-print("branch test 8")
 print(a % b)
 
 import math
-print("branch test 9")
 print(math.ceil(a / b))
-
-print("branch test 10")
-print("branch test 9")
-
-print(a ** b)
-print("branch test 10")
-
-b = 123 - 456 / 98
-print(b)
-print("branch test 11")
 
 def plus(x: int, y: int) -> int:
     return x + y
